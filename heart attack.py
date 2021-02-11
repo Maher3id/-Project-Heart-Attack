b6e84c45deba4e326c7876432698c1b8b78b21d1
Maher Eid <76223891+Maher3id@users.noreply.github.com>
2021-02-11 20:13:03 +0200

    Update heart attack.py

--- heart attack.py
+++ heart attack.py
@@ -25,7 +25,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
 
 
-#training the random forest regression model
+#training the random forest Classifier model
 from sklearn.ensemble import RandomForestClassifier
 regressor=RandomForestClassifier(n_estimators=300,random_state=0)
 regressor.fit(X_train,y_train)
