af5d898986caf17f36b6518a7b1bab6b763a8099
Maher Eid <76223891+Maher3id@users.noreply.github.com>
2021-02-12 20:04:06 +0200

    Update heart attack.py

--- heart attack.py
+++ heart attack.py
@@ -1,36 +1,131 @@
-#importing the libraries
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-#importing the dataset
-dataset=pd.read_csv('D:/Breast Cancer.csv')
-X=dataset.iloc[:,2:32].values
-y=dataset.iloc[:,1].values
-
-
-#feature scaling
-from sklearn.preprocessing import StandardScaler
-sc_X=StandardScaler()
-X=sc_X.fit_transform(X)
-
-#encoding categorical data
-from sklearn.preprocessing import LabelEncoder
-le=LabelEncoder()
-y=le.fit_transform(y)
-
-#spiliting the dataset into the trining set and test set
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+from matplotlib import rcParams
+from matplotlib.cm import rainbow
+get_ipython().run_line_magic('matplotlib', 'inline')
+import warnings
+warnings.filterwarnings('ignore')
 
 
+# In[2]:
 
-#training the random forest Classifier model
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-regressor=RandomForestClassifier(n_estimators=300,random_state=0)
-regressor.fit(X_train,y_train)
 
-#predict a new result
-y_pred=regressor.predict(X_test)
 
-print(f'{regressor.score(X_test,y_test):0.2%}')
+# In[4]:
+
+
+df = pd.read_csv('heartattack.csv')
+
+
+# In[5]:
+
+
+df.info()
+
+
+# In[6]:
+
+
+df.describe()
+
+
+# In[7]:
+
+
+import seaborn as sns
+#get correlations of each features in dataset
+corrmat = df.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(20,20))
+#plot heat map
+g=sns.heatmap(df[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+
+
+# In[8]:
+
+
+df.hist()
+
+
+# In[9]:
+
+
+sns.set_style('whitegrid')
+sns.countplot(x='target',data=df,palette='RdBu_r')
+
+
+# In[10]:
+
+
+dataset = pd.get_dummies(df, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
+
+
+# In[11]:
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+dataset[columns_to_scale] = standardScaler.fit_transform(dataset[columns_to_scale])
+
+
+# In[12]:
+
+
+dataset.head()
+
+
+# In[14]:
+
+
+y = dataset['target']
+X = dataset.drop(['target'], axis = 1)
+from sklearn.model_selection import cross_val_score
+knn_scores = []
+for k in range(1,21):
+    knn_classifier = KNeighborsClassifier(n_neighbors = k)
+    score=cross_val_score(knn_classifier,X,y,cv=10)
+    knn_scores.append(score.mean())
+plt.plot([k for k in range(1, 21)], knn_scores, color = 'red')
+for i in range(1,21):
+    plt.text(i, knn_scores[i-1], (i, knn_scores[i-1]))
+plt.xticks([i for i in range(1, 21)])
+plt.xlabel('Number of Neighbors (K)')
+plt.ylabel('Scores')
+plt.title('K Neighbors Classifier scores for different K values')
+
+
+# In[15]:
+
+
+knn_classifier = KNeighborsClassifier(n_neighbors = 12)
+score=cross_val_score(knn_classifier,X,y,cv=10)
+score.mean()
+
+
+# In[16]:
+
+
+from sklearn.ensemble import RandomForestClassifier
+randomforest_classifier= RandomForestClassifier(n_estimators=10)
+score=cross_val_score(randomforest_classifier,X,y,cv=10)
+score.mean()
+
+
+# In[ ]:
+
+
+
+
